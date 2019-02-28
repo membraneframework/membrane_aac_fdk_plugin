f289be3cdec9839d728441576fda0d5902e0f70d
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2019-02-28 13:37:54 +0100

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,60 @@
+defmodule Membrane.Element.AAC.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+  @github_url "https://github.com/membraneframework/membrane-element-aac"
+
+  def project do
+    [
+      app: :membrane_element_aac,
+      version: @version,
+      elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      description: "Membrane Multimedia Framework (AAC Element)",
+      package: package(),
+      name: "Membrane Element: AAC",
+      source_url: @github_url,
+      docs: docs(),
+      homepage_url: "https://membraneframework.org",
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github_url,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      },
+      files: ["lib", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs", "c_src"]
+    ]
+  end
+
+  defp deps do
+    [
+      {:membrane_core, "~> 0.2.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
+    ]
+  end
+end
