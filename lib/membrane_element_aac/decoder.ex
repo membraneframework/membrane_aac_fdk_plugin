2bf757639b45dd0ccd783c575d118692734e5633
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2019-03-05 15:46:46 +0100

    Minor improvements to decoder

--- lib/membrane_element_aac/decoder.ex
+++ lib/membrane_element_aac/decoder.ex
@@ -54,7 +54,7 @@ defmodule Membrane.Element.AAC.Decoder do
   to ensure we're emptying the whole native buffer.
   3. Set output caps based on the stream metadata.
   This should execute only once when output caps are not specified yet,
-  since they should stay consistent forthe whole stream.
+  since they should stay consistent for the whole stream.
   4. In case an unhandled error is returned during this flow, returns error message.
   """
   @impl true
@@ -62,7 +62,7 @@ defmodule Membrane.Element.AAC.Decoder do
     with {:ok} <- Native.fill(payload, state.native),
          {:ok, decoded_frames} <- decode_buffer(payload, state.native),
          {:ok, caps_action} <- get_caps_if_needed(ctx.pads.output.caps, state) do
-      buffer_actions = wrap_frames(decoded_frames)
+      buffer_actions = [buffer: {:output, [wrap_frames(decoded_frames)]}]
 
       {{:ok, caps_action ++ buffer_actions ++ [redemand: :output]}, state}
     else
@@ -77,21 +77,19 @@ defmodule Membrane.Element.AAC.Decoder do
     case Native.decode_frame(payload, native) do
       {:ok, decoded_frame} ->
         # Accumulate decoded frames
-        decode_buffer(payload, native, acc ++ [decoded_frame])
+        decode_buffer(payload, native, [decoded_frame | acc])
 
       {:error, :not_enough_bits} ->
         # Means that we've parsed the whole buffer.
-        {:ok, acc}
+        {:ok, Enum.reverse(acc)}
 
       {:error, reason} ->
         {:error, reason}
     end
   end
 
-  defp wrap_frames([]), do: []
-
   defp wrap_frames(frames) do
-    frames |> Enum.map(fn frame -> %Buffer{payload: frame} end) ~> [buffer: {:output, &1}]
+    frames |> Enum.map(fn frame -> %Buffer{payload: frame} end)
   end
 
   defp get_caps_if_needed(nil, state) do
