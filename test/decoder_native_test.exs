3c86e4042c8277374c7d52dbd1915d531ecde77e
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2019-03-08 10:44:12 +0100

    Add more Native AAC Decoder unit tests

--- test/decoder_native_test.exs
+++ test/decoder_native_test.exs
@@ -2,23 +2,81 @@ defmodule Decoder.NativeTest do
   use ExUnit.Case, async: true
   alias Membrane.Element.AAC.Decoder.Native
 
-  test "Decode 1 AAC frame" do
-    in_path = "fixtures/input-sample.aac" |> Path.expand(__DIR__)
-    reference_path = "fixtures/reference-sample.raw" |> Path.expand(__DIR__)
+  def prepare_paths(filename) do
+    in_path = "fixtures/input-#{filename}.aac" |> Path.expand(__DIR__)
+    reference_path = "fixtures/reference-#{filename}.raw" |> Path.expand(__DIR__)
+    {in_path, reference_path}
+  end
 
-    assert {:ok, file} = File.read(in_path)
-    assert {:ok, decoder_ref} = Native.create()
+  def assert_frames_equal(decoded_frame, reference_frame) do
+    assert bit_size(decoded_frame) == bit_size(reference_frame)
+    assert Membrane.Payload.to_binary(decoded_frame) == reference_frame
+  end
 
-    assert <<frame::bytes-size(256), _::binary>> = file
-    assert :ok = Native.fill(frame, decoder_ref)
-    assert {:ok, decoded_frame} = Native.decode_frame(frame, decoder_ref)
-    assert {:error, :not_enough_bits} = Native.decode_frame(frame, decoder_ref)
+  describe "Native AAC Decoder should" do
+    test "Decode 1 AAC frame" do
+      {in_path, reference_path} = prepare_paths("sample")
 
-    assert {:ok, ref_file} = File.read(reference_path)
+      assert {:ok, file} = File.read(in_path)
+      assert {:ok, decoder_ref} = Native.create()
 
-    assert <<ref_frame::bytes-size(4096), _::binary>> = ref_file
+      assert <<frame::bytes-size(256), _::binary>> = file
+      assert :ok = Native.fill(frame, decoder_ref)
+      assert {:ok, decoded_frame} = Native.decode_frame(frame, decoder_ref)
+      assert {:error, :not_enough_bits} = Native.decode_frame(frame, decoder_ref)
 
-    assert bit_size(decoded_frame) == bit_size(ref_frame)
-    assert Membrane.Payload.to_binary(decoded_frame) == ref_frame
+      assert {:ok, ref_file} = File.read(reference_path)
+
+      assert <<ref_frame::bytes-size(4096), _::binary>> = ref_file
+
+      assert_frames_equal(decoded_frame, ref_frame)
+    end
+
+    test "Decode multiple AAC frames" do
+      {in_path, reference_path} = prepare_paths("sample")
+
+      assert {:ok, file} = File.read(in_path)
+      assert {:ok, decoder_ref} = Native.create()
+
+      assert <<frame::bytes-size(1024), _::binary>> = file
+      assert :ok = Native.fill(frame, decoder_ref)
+      assert {:ok, decoded_frame1} = Native.decode_frame(frame, decoder_ref)
+      assert {:ok, decoded_frame2} = Native.decode_frame(frame, decoder_ref)
+      assert {:ok, decoded_frame3} = Native.decode_frame(frame, decoder_ref)
+      assert {:ok, decoded_frame4} = Native.decode_frame(frame, decoder_ref)
+      assert {:error, :not_enough_bits} = Native.decode_frame(frame, decoder_ref)
+
+      assert {:ok, ref_file} = File.read(reference_path)
+
+      assert <<ref_frame1::bytes-size(4096), ref_frame2::bytes-size(4096),
+               ref_frame3::bytes-size(4096), ref_frame4::bytes-size(4096), _::binary>> = ref_file
+
+      assert_frames_equal(decoded_frame1, ref_frame1)
+      assert_frames_equal(decoded_frame2, ref_frame2)
+      assert_frames_equal(decoded_frame3, ref_frame3)
+      assert_frames_equal(decoded_frame4, ref_frame4)
+    end
+
+    test "Decode chunked AAC frame" do
+      {in_path, reference_path} = prepare_paths("sample")
+
+      assert {:ok, file} = File.read(in_path)
+      assert {:ok, decoder_ref} = Native.create()
+
+      assert <<chunk1::bytes-size(128), chunk2::bytes-size(128), _::binary>> = file
+
+      assert :ok = Native.fill(chunk1, decoder_ref)
+      # First chunk does not contain a full frame
+      assert {:error, :not_enough_bits} = Native.decode_frame(chunk1, decoder_ref)
+
+      assert :ok = Native.fill(chunk2, decoder_ref)
+      # Only after the second chunk is filled we can decode the frame
+      assert {:ok, decoded_frame} = Native.decode_frame(chunk2, decoder_ref)
+      assert {:ok, ref_file} = File.read(reference_path)
+
+      assert <<ref_frame::bytes-size(4096), _::binary>> = ref_file
+
+      assert_frames_equal(decoded_frame, ref_frame)
+    end
   end
 end
