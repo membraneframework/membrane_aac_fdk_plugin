355dace966a77ff2b0602367bea15c207480e87a
Krzysztof Ciombor <krzysztof.ciombor@swmansion.com>
2019-03-13 13:18:47 +0100

    Add AAC Encoder tests

---
+++ test/encoder_test.exs
@@ -0,0 +1,33 @@
+defmodule EncoderTest do
+  use ExUnit.Case
+
+  import Membrane.Testing.Pipeline.Assertions
+
+  alias Membrane.Pipeline
+
+  def assert_files_equal(file_a, file_b) do
+    assert {:ok, a} = File.read(file_a)
+    assert {:ok, b} = File.read(file_b)
+    assert a == b
+  end
+
+  def prepare_paths(filename) do
+    in_path = "fixtures/input-#{filename}.raw" |> Path.expand(__DIR__)
+    reference_path = "fixtures/reference-#{filename}.aac" |> Path.expand(__DIR__)
+    out_path = "/tmp/output-encoding-#{filename}.aac"
+    File.rm(out_path)
+    on_exit(fn -> File.rm(out_path) end)
+    {in_path, reference_path, out_path}
+  end
+
+  describe "Encoding Pipeline should" do
+    test "Encode AAC file" do
+      {in_path, reference_path, out_path} = prepare_paths("encoder")
+      assert {:ok, pid} = EncodingPipeline.make_pipeline(in_path, out_path)
+
+      assert Pipeline.play(pid) == :ok
+      assert_receive_message({:handle_notification, {{:end_of_stream, :input}, :sink}}, 3000)
+      assert_files_equal(out_path, reference_path)
+    end
+  end
+end
---
+++ test/support/encoding_pipeline.ex
@@ -0,0 +1,17 @@
+defmodule EncodingPipeline do
+  @moduledoc false
+
+  alias Membrane.Testing.Pipeline
+
+  def make_pipeline(in_path, out_path, pid \\ self()) do
+    Pipeline.start_link(%Pipeline.Options{
+      elements: [
+        file_src: %Membrane.Element.File.Source{location: in_path},
+        encoder: %Membrane.Element.AAC.Encoder{input_caps: %{sample_rate: 44_100, channels: 2}},
+        sink: %Membrane.Element.File.Sink{location: out_path}
+      ],
+      monitored_callbacks: [:handle_notification],
+      test_process: pid
+    })
+  end
+end
