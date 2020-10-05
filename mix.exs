98156f6bd641f3750b06b62db6377d8ec2f88a54
Mateusz Front <mateusz.front@swmansion.com>
2020-10-05 14:32:00 +0200

    bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.FDK.AAC.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @github_url "https://github.com/membraneframework/membrane-element-fdk-aac"
 
   def project do
