7b839b476ea56151f6e2c01fc06893b683fe9b3f
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-06-07 11:52:22 +0200

    bump to 0.15.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AAC.FDK.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.14.0"
+  @version "0.15.0"
   @github_url "https://github.com/membraneframework/membrane_aac_fdk_plugin"
 
   def project do
@@ -56,8 +56,8 @@ defmodule Membrane.AAC.FDK.Plugin.MixProject do
   defp deps do
     [
       {:bunch, "~> 1.4"},
-      {:membrane_core, "~> 0.11.0"},
-      {:membrane_common_c, "~> 0.14.0"},
+      {:membrane_core, "~> 0.12.0"},
+      {:membrane_common_c, "~> 0.15.0"},
       {:unifex, "~> 1.0"},
       {:membrane_raw_audio_format, "~> 0.10.0"},
       {:membrane_aac_format, "~> 0.7"},
