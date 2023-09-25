80a2af14c60f2e118b54e466623dab1078353963
Noarkhh <jakub.pryc@gmail.com>
2023-09-25 14:24:06 +0200

    Use precompiled fdk-aac, update fixtures

--- bundlex.exs
+++ bundlex.exs
@@ -7,20 +7,35 @@ defmodule Membrane.AAC.FDK.BundlexProject do
     ]
   end
 
+  defp get_fdk_aac_url() do
+    case Bundlex.get_target() do
+      %{os: "linux"} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/download/version1/fdk_aac_linux.tar.gz"}
+
+      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/download/version1/fdk_aac_macos_intel.tar.gz"}
+
+      _other ->
+        nil
+    end
+  end
+
   def natives() do
     [
       decoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["decoder.c"],
-        pkg_configs: ["fdk-aac"],
+        os_deps: [{get_fdk_aac_url(), "fdk-aac"}],
         preprocessor: Unifex
       ],
       encoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["encoder.c"],
-        pkg_configs: ["fdk-aac"],
+        os_deps: [{get_fdk_aac_url(), "fdk-aac"}],
         preprocessor: Unifex
       ]
     ]
