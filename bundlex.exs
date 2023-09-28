e5d64cf3031887d8d6699a4ee5256c1211ea3274
Noarkhh <jakub.pryc@gmail.com>
2023-09-28 17:39:16 +0200

    Support M1 architecture

--- bundlex.exs
+++ bundlex.exs
@@ -8,18 +8,16 @@ defmodule Membrane.AAC.FDK.BundlexProject do
   end
 
   defp get_fdk_aac_url() do
-    case Bundlex.get_target() do
-      %{os: "linux"} ->
-        {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/download/version1/fdk_aac_linux.tar.gz"}
+    system_architecture =
+      case Bundlex.get_target() do
+        %{os: "linux"} -> "linux"
+        %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} -> "macos_intel"
+        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} -> "macos_m1"
+        _other -> nil
+      end
 
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/download/version1/fdk_aac_macos_intel.tar.gz"}
-
-      _other ->
-        nil
-    end
+    {:precompiled,
+     "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/latest/download/fdk-aac_#{system_architecture}.tar.gz"}
   end
 
   def natives() do
