741e3666bce19aea5312aedb45b896c154b95c62
Noarkhh <jakub.pryc@gmail.com>
2023-10-06 12:30:43 +0200

    Add pragmas for gcc

--- c_src/membrane_aac_fdk_plugin/decoder.h
+++ c_src/membrane_aac_fdk_plugin/decoder.h
@@ -3,6 +3,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wextra" 
+#pragma GCC diagnostic ignored "-Wunused-function" 
 #include <fdk-aac/aacdecoder_lib.h>
 #pragma GCC diagnostic pop
 
