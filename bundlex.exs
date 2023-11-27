6e5da11d142d3f4ec475b18cbb74b6b0ae27015c
Mateusz Front <mfront3@gmail.com>
2023-11-27 12:49:58 +0100

    Fix bundlex and unifex dependency (#49)
    * Fix bundlex and unifex dependency
    
    * Update README.md
    
    * update to bundlex new API

--- bundlex.exs
+++ bundlex.exs
@@ -7,39 +7,42 @@ defmodule Membrane.AAC.FDK.BundlexProject do
     ]
   end
 
-  defp get_fdk_aac_url() do
+  defp get_fdk_aac() do
     url_prefix =
       "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/latest/download/fdk-aac"
 
-    case Bundlex.get_target() do
-      %{os: "linux"} ->
-        {:precompiled, "#{url_prefix}_linux.tar.gz"}
+    url =
+      case Bundlex.get_target() do
+        %{os: "linux"} ->
+          "#{url_prefix}_linux.tar.gz"
 
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{url_prefix}_macos_intel.tar.gz"}
+        %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+          "#{url_prefix}_macos_intel.tar.gz"
 
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        {:precompiled, "#{url_prefix}_macos_arm.tar.gz"}
+        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
+          "#{url_prefix}_macos_arm.tar.gz"
 
-      _other ->
-        nil
-    end
+        _other ->
+          nil
+      end
+
+    [{:precompiled, url}, :pkg_config]
   end
 
-  def natives() do
+  defp natives() do
     [
       decoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["decoder.c"],
-        os_deps: [{[get_fdk_aac_url(), :pkg_config], "fdk-aac"}],
+        os_deps: ["fdk-aac": get_fdk_aac()],
         preprocessor: Unifex
       ],
       encoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["encoder.c"],
-        os_deps: [{[get_fdk_aac_url(), :pkg_config], "fdk-aac"}],
+        os_deps: ["fdk-aac": get_fdk_aac()],
         preprocessor: Unifex
       ]
     ]
