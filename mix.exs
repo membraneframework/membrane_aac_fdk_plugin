0b076d152f7af26b1d8b8b740e7d3a05d2ae764f
Mateusz Front <mfront3@gmail.com>
2023-11-27 12:23:17 +0100

    Fix bundlex and unifex dependency

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AAC.FDK.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.18.0"
+  @version "0.18.1"
   @github_url "https://github.com/membraneframework/membrane_aac_fdk_plugin"
 
   def project do
@@ -58,8 +58,8 @@ defmodule Membrane.AAC.FDK.Plugin.MixProject do
       {:bunch, "~> 1.4"},
       {:membrane_core, "~> 1.0"},
       {:membrane_common_c, "~> 0.16.0"},
-      {:unifex, "~> 1.1.0"},
-      {:bundlex, "~> 1.2.0"},
+      {:unifex, "~> 1.1"},
+      {:bundlex, "~> 1.2"},
       {:membrane_raw_audio_format, "~> 0.12.0"},
       {:membrane_aac_format, "~> 0.8.0"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
