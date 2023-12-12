84fc56f35a3cc5a5f6aabebb6183ccafb41d7710
Noarkhh <jakub.pryc@gmail.com>
2023-12-12 12:23:55 +0100

    Start using Membrane.PrecompiledDependencyProvider

--- bundlex.exs
+++ bundlex.exs
@@ -7,42 +7,30 @@ defmodule Membrane.AAC.FDK.BundlexProject do
     ]
   end
 
-  defp get_fdk_aac() do
-    url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_fdk_aac/releases/latest/download/fdk-aac"
-
-    url =
-      case Bundlex.get_target() do
-        %{os: "linux"} ->
-          "#{url_prefix}_linux.tar.gz"
-
-        %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-          "#{url_prefix}_macos_intel.tar.gz"
-
-        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-          "#{url_prefix}_macos_arm.tar.gz"
-
-        _other ->
-          nil
-      end
-
-    [{:precompiled, url}, :pkg_config]
-  end
-
   defp natives() do
     [
       decoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["decoder.c"],
-        os_deps: ["fdk-aac": get_fdk_aac()],
+        os_deps: [
+          "fdk-aac": [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:"fdk-aac")},
+            :pkg_config
+          ]
+        ],
         preprocessor: Unifex
       ],
       encoder: [
         interface: :nif,
         deps: [membrane_common_c: :membrane],
         sources: ["encoder.c"],
-        os_deps: ["fdk-aac": get_fdk_aac()],
+        os_deps: [
+          "fdk-aac": [
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:"fdk-aac")},
+            :pkg_config
+          ]
+        ],
         preprocessor: Unifex
       ]
     ]
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.AAC.FDK.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.18.1"
+  @version "0.18.2"
   @github_url "https://github.com/membraneframework/membrane_aac_fdk_plugin"
 
   def project do
@@ -60,6 +60,7 @@ defmodule Membrane.AAC.FDK.Plugin.MixProject do
       {:membrane_common_c, "~> 0.16.0"},
       {:unifex, "~> 1.1"},
       {:bundlex, "~> 1.3"},
+      {:membrane_precompiled_dependency_provider, "~> 0.1.0"},
       {:membrane_raw_audio_format, "~> 0.12.0"},
       {:membrane_aac_format, "~> 0.8.0"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
