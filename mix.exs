0f4b626159ac81447f2adc96d6064ccb7d0138d7
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 11:25:48 +0200

    Set ex_doc dependency to >= 0.40.0

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Membrane.AAC.FDK.Plugin.MixProject do
       {:membrane_raw_audio_format, "~> 0.12.0"},
       {:membrane_aac_format, "~> 0.8.0"},
       {:membrane_file_plugin, "~> 0.16.0", only: :test},
-      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.40.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false}
     ]
